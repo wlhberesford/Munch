f62d11af3f645ed0b76659f1ae9de878b69faca3
SUS <101779634+rgc14@users.noreply.github.com>
2024-02-13 16:22:57 -0500

    fixed urls

--- munch_backend/munchapp/urls.py
+++ munch_backend/munchapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="menu-home"),
+    path('menu/sage/', views.menu_sage, name="menu_sage"),
+    path('about/', views.about, name="about"),
 ]
--- munch_backend/munchapp/views.py
+++ munch_backend/munchapp/views.py
@@ -6,7 +6,34 @@ from django.http import HttpResponse
 
 from django.template import loader
 
-def home(request):
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+import requests
 
+from bs4 import BeautifulSoup
+
+def get_hrs():
+    
+    #THIS DOES NOT WORK YET
+
+    hrs = []
+
+    url = 'https://rpi.sodexomyway.com/dining-near-me/russell-sage'
+    data = requests.get(url)
+    html = BeautifulSoup(data.text, 'html.parser')
+    times = html.select('todays-hrs-block')
+
+    print(times)
+
+    for time in times:
+        start_time = time.select('start-hours').get_text()
+        end_time = time.select('end-hours').get_text()
+
+        hrs.append(start_time + end_time)
+
+    return hrs
+
+def menu_sage(request):
+    print(get_hrs())
+    return render(request, 'munchapp/menu.html')
+
+def about(request):
+    return HttpResponse('<h1>rcos munch dining hall project rpi</h1>')
