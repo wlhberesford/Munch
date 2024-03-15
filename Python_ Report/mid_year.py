c66a476065652d170bf834ada21c42c01741c714
Liam Beresford <wlhberesford@gmail.com>
2024-03-15 17:08:28 -0400

    Finished Midyear report script

--- Python_ Report/mid_year.py
+++ Python_ Report/mid_year.py
@@ -10,22 +10,26 @@ different folder and use there.
 """
 
 
-input_file=""
+input_file="git_log.txt"
 
 
-
+git_username="wlhberesford"
 
 in_f=open(input_file)
-out_f=open("format.txt")
+out_f=open("format.txt","w")
 
+commits=[]
 
 for line in in_f:
     words=line.split(" ")
-    if(words.count("commit")==1):
+    if(words.count("commit")>=1):
         loc=words.index("commit")
-        loc+=1
-        in_f.write("https://github.com/wlhberesford/Munch/commit/{}".format(words[loc]))
+        commits.append("https://github.com/{}/Munch/commit/{}".format(git_username,words[1]).strip())
+        commits.append("\n")
     else:
         pass
         
-        
+out_f.writelines(commits)
+
+in_f.close()
+out_f.close()
