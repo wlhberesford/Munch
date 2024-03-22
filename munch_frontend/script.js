5afba0a7a7ce246bdea9aa4edc41c16be9d9dac4
Liam Beresford <wlhberesford@gmail.com>
2024-03-22 18:30:41 -0400

    Adde js file

---
+++ munch_frontend/script.js
@@ -0,0 +1,25 @@
+
+
+function getCurrentMeal(){
+    const d = new Date();
+    var hour = d.getHours();
+    console.log(hour);
+    if ( 7< hour && hour < 11){
+        document.getElementById("current_food_title").innerHTML="What's For Breakfast?";
+        console.log(hour);
+
+    }else if ( hour < 16){
+        document.getElementById("current_food_title").innerHTML="What's For Lunch?";
+
+    } else if ( hour < 21){
+        document.getElementById("current_food_title").innerHTML="What's For Dinner?";
+
+    }else{
+        document.getElementById("current_food_title").innerHTML="Yout gotta wait for food my boi";
+
+    }
+    
+    
+
+    
+}
