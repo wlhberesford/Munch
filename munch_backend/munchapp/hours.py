9dae7f5c1cd31643f008d2b107b9e498cde3d711
tenzinpalden <tpalden2003@gmail.com>
2024-03-22 16:53:55 -0400

    Comments and changed testing

--- munch_backend/munchapp/hours.py
+++ munch_backend/munchapp/hours.py
@@ -17,19 +17,24 @@ def parse_information(soup):
     locations_hours = {}
     dining_groups = soup.find_all("li", class_="dining-group")
 
+    # Iterate through each dining group
     for group in dining_groups:
         group_name = group.find('h2').text
         dining_locations = group.find_all("div", class_="dining-block")
+        # Iterate through each dining location within the group
         for location in dining_locations:
             location_name = location.find('h3').text
+            print(location_name)
             regular_hours = location.find("div", class_="reghours")
             special_hours = location.find_all("div", class_="spechours")
             location_hours = []
+            # Extract regular hours if available
             if regular_hours:
                 regular_days_tag = regular_hours.find_all("dt", {"data-arrayregdays": True})
                 regular_times = regular_hours.find_all("span", class_="dining-block-hours")
                 for days_tag, time in zip(regular_days_tag, regular_times):
                     location_hours.append(f"{days_tag['data-arrayregdays']}: {time.text.strip()}")
+            # Extract special hours if available
             if special_hours:
                 for special_hour in special_hours:
                     special_dates = special_hour.find_all("span", class_="specweekstart")
@@ -40,6 +45,7 @@ def parse_information(soup):
                     special_times = special_hour.find_all("p", class_="dining-block-hours")
                     for days, times in zip(special_days, special_times):
                         location_hours.append(f"{days.text.strip()}: {times.text.strip()}")
+            # Store the location hours in the dictionary
             locations_hours[location_name] = location_hours
     return locations_hours
 
@@ -64,10 +70,9 @@ def main():
     # Parse information and save location and hours in a data structure
     locations_hours = parse_information(soup)
     # Get hours for a specific location
-    location_name = "The Commons Dining Hall"  # Replace "Location Name" with the desired location
+    location_name = input("Location: ")  # Replace "Location Name" with the desired location
     hours = get_hours(location_name, locations_hours)
     if hours:
-        print(f"Hours for {location_name}:")
         for hour in hours:
             print(hour)
     else:
