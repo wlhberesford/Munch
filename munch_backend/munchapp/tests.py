d4acf87076c2d9d4dfbf085f0263e1206bfc17ce
jaswanthDuddu <57510269+jaswanthDuddu@users.noreply.github.com>
2024-03-29 17:21:35 -0400

    modified:   munch_backend/munchapp/tests.py

--- munch_backend/munchapp/tests.py
+++ munch_backend/munchapp/tests.py
@@ -4,4 +4,11 @@ from django.test import TestCase
 
 """
 Writing some small test cases to ensure that the views.py file is working correctly
+uncomment for each test case
 """
+
+# a = get_hrs("russell-sage")
+
+# check = ("Breakfast: 7:00AM - 10:00AM", "Continental Breakfast: 10:00AM - 11:00AM", "Lunch: 11:00AM - 2:30PM", "Dinner: 4:00PM - 8:00PM")
+
+# assert(a == check)
